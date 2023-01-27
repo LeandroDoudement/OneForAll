e99c8b8527a0b4adc63ca9bc53bf4296a3f86f70
LeandroDoudement <leandrodoudement@gmail.com>
2023-01-27 19:32:28 -0300

    Requisito 11 e projeto finalizado

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+SELECT song_name AS nome_musica, CASE
+WHEN song_name LIKE '%Bard%' THEN REPLACE(song_name, 'Bard', 'QA')
+WHEN song_name LIKE '%Amar%' THEN REPLACE(song_name, 'Amar', 'Code Review')
+WHEN song_name LIKE '%Pais%' THEN REPLACE(song_name, 'Pais', 'Pull Requests')
+WHEN song_name LIKE '%SOUL%' THEN REPLACE(song_name, 'SOUL', 'CODE')
+WHEN song_name LIKE '%SUPERSTAR%' THEN REPLACE(song_name, 'SUPERSTAR', 'SUPERDEV')
+END AS novo_nome
+FROM SpotifyClone.Songs
+HAVING novo_nome IS NOT NULL
+ORDER BY song_name DESC;
